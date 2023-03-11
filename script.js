1d73deb5530bba401179060aa6250dc7565f0b97
Deividas <deividas.aldonis.dev@gmail.com>
2023-03-11 19:28:19 +0200

    finish desktop mega menu

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const mobileNav = document.querySelector(".navbar__nav-mobile");
 const mobileNavHamburgerBtn = document.querySelector(".navbar__hamburger-btn");
 const accordionBtns = document.querySelectorAll(".navbar__accordion-btn");
 
+const subMenuCategoryBtns = document.querySelectorAll(".sub-menu-category");
+const featuredTemplates = document.querySelectorAll(
+  ".featured-templates-wrapper"
+);
+
 mobileNavHamburgerBtn.addEventListener("click", openMenu);
 
 accordionBtns.forEach((btn) => {
@@ -29,4 +34,24 @@ function openAccordion(e) {
   } else {
     accordion.classList.add("hide");
   }
+}
+
+subMenuCategoryBtns.forEach((btn) => {
+  btn.addEventListener("click", switchTemplate);
+});
+
+function switchTemplate(e) {
+  const target = e.currentTarget;
+  subMenuCategoryBtns.forEach((btn) => btn.classList.remove("active"));
+
+  target.classList.add("active");
+
+  const targetFeaturedTemplate = [...featuredTemplates].find(
+    (featuredTemplate) => {
+      return featuredTemplate.dataset.template === target.dataset.template;
+    }
+  );
+  featuredTemplates.forEach((t) => t.classList.remove("active"));
+
+  targetFeaturedTemplate.classList.add("active");
 }
